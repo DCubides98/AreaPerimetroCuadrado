2c9d925f9f0f62f8a537d5bee483b2a9ae771ced
ECM9749E <sebas@MacBook-Air-de-Sebastian.local>
2023-11-15 00:01:02 -0500

    Area perimetro cuadrado

---
+++ src/AreaPerimetro.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class AreaPerimetro {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("POR FAVOR INGRESE EL LADO DE UN CUADRADO: ");
+
+        double lado = scanner.nextDouble();
+
+        double area = lado * lado;
+        double perimetro = 4 * lado;
+
+        System.out.println("El area del cuadrado es: " + area);
+        System.out.println("El area del perimetro es: " + perimetro);
+
+        scanner.close();
+    }
+
+}
